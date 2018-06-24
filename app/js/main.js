81c109381307162f32f72a219d08ca1b14d91e88
Kristaps Grinbergs <kristaps@qminderapp.com>
2018-06-24 18:03:22 +0300

    Added tracking

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
-const SKILLS = [["Swift, Objective-C", 98],["Xcode, Cocoapods, Carthage", 97], ["iOS, tvOS, watchOS, macOS", 98], ["Buddybuild, Circle CI, Travis CI", 92],["RxSwift, MVVM", 90],["Javascript, Angular, jQuery", 82], ["HTML, CSS", 90], ["PostgreSQL, Oracle, MongoDB, SQlite", 84], ["Java, Ruby, Python, PHP", 72],["Sketch", 70]];
+const SKILLS = [
+  ["Swift, Objective-C", 98],
+  ["Xcode, Cocoapods, Carthage", 97],
+  ["iOS, tvOS, watchOS, macOS", 98],
+  ["Buddybuild, Circle CI, Travis CI", 92],
+  ["RxSwift, MVVM", 90],
+  ["Javascript, Angular, jQuery", 82],
+  ["HTML, CSS", 90],
+  ["PostgreSQL, Oracle, MongoDB, SQlite", 84],
+  ["Java, Ruby, Python, PHP", 72],
+  ["Sketch", 70]
+];
 
 const SKILLS_CONT = document.getElementById("skills__section")
 
@@ -12,6 +23,14 @@ function renderSkilltoDOM(skill) {
     </div>`
   );
 }
-  SKILLS.forEach((skill) => {
-    renderSkilltoDOM(skill);
-  });
+
+SKILLS.forEach((skill) => {
+  renderSkilltoDOM(skill);
+});
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-87318818-1', 'auto');
+  ga('send', 'pageview');
