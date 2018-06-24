9654d915a093fbc33e059ab57549adb084ac3970
Kristaps Grinbergs <kristaps@qminderapp.com>
2018-06-24 14:28:00 +0300

    Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -86,12 +86,12 @@ gulp.task('build', function(callback) {
 
 
 gulp.task('default', () =>
-    gulp.src('app/css/*.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-    .pipe(gulp.dest('dist'))
+  gulp.src('app/css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+  .pipe(gulp.dest('dist'))
 );
 
 gulp.task('deploy', function(cb) {
